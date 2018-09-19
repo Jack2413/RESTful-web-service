ac8f844ec026be170b23c83cb87684eecddbc771
Jack <jackgan@JackdeMacBook-Pro.local>
2018-09-20 00:43:13 +1200

    s

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/get', async (req, res) => {
 			result.rows.forEach(row=>{ console.log(row);
 			});
 		}
-	res.render('front-end', {'tasks': result.rows});
+	res.render('index', {'tasks': result.rows});
 	client.release();
 	} catch (err) { 
 		console.error(err); 
